2e7387f78580044331cdac0b25f0640edd63c084
Werton <wertonf4bian@gmail.com>
2023-11-17 23:17:57 -0300

    js script

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+/* navbar variables*/
+
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
